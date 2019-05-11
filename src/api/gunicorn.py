5a5a18d3ea24bc6c410293a2558e2d1b03988465
Rusty Brooks <rbrooks@alienvault.com>
2019-05-11 18:18:22 -0500

    use threaded worker class

--- src/api/gunicorn.py
+++ src/api/gunicorn.py
@@ -4,7 +4,8 @@ workers = 2
 accesslog = '/srv/logs/gunicorn_access.log'
 errorlog = ' /srv/logs/gunicorn_errors.log'
 preload_app = True
-worker_class = 'gevent'
+# worker_class = 'gevent'
+worker_class = 'gthread'
 
 # max_requests = 10000
 # max_requests_jitter = int(max_requests / 2)
