c88776196f1fff6273f5cca72a1990402543aed9
rustybrooks <me@rustybrooks.com>
2024-10-04 11:36:31 -0500

    redo a bunch of stuff to django

--- src/ui/src/constants/api.ts
+++ src/ui/src/constants/api.ts
@@ -1,6 +1,6 @@
 const hostname = window && window.location.origin;
 
-export const REMOTE_API = 'https://bikes-api.prod.rustybrooks.net';
+export const REMOTE_API = 'https://bikes.rustybrooks.com';
 export const LOCAL_API = 'http://localhost:3000';
 
 export const BASE_URL = hostname.includes('bikes') ? REMOTE_API : LOCAL_API;
